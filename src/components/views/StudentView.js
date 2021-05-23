9933c7e0861ef53c751438ea1c179ded8c76299c
Li Gao <ligao0807@gmail.com>
2021-05-23 16:21:13 -0400

    fail to add corresponding campus in single student page

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const StudentsView = (props) => {
     const {student} = props;
@@ -5,10 +6,11 @@ const StudentsView = (props) => {
   return (
     <div>      
       <h1>{student.firstname} {student.lastname}</h1>
-      <img src={student.imageUrl} alt="student' image" width="150" height="150" />
+      <img src={student.imageUrl} alt="student's image" width="150" height="150" />
       <p>Email: {student.email}</p>
-      <p>GPA: {student.gpa}</p>   
-     </div>
+      <p>GPA: {student.gpa}</p>  
+      <p>{student.campusId}</p>
+    </div>
     );
 };
 
